80727cf9c5e2317ee1cf234e0577046641971fa5
Kumar R N <kumar_reddy_n@outlook.com>
2021-11-21 18:07:28 +0530

    Initial commit

---
+++ src/test/java/org/tekloka/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tekloka.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
