66df08a3ebb5f9b32e7cf89bb3defc394ed125a8
Kumar R N <kumar_reddy_n@outlook.com>
2021-11-30 11:20:45 +0530

    Bug fix - Jasypt decypt

--- src/main/java/org/tekloka/config/server/ConfigServerApplication.java
+++ src/main/java/org/tekloka/config/server/ConfigServerApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
 @EnableConfigServer
+@EnableEncryptableProperties
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
